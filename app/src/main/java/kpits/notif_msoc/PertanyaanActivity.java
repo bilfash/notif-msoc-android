00a8d483f390d1a13f4137ff63b6e12f5263d4b8
Herdianto Naufal Farras <fherdiantonaufal@yahoo.com>
2016-08-09 10:36:13 +0700

    Back end response

--- app/src/main/java/kpits/notif_msoc/PertanyaanActivity.java
+++ app/src/main/java/kpits/notif_msoc/PertanyaanActivity.java
@@ -1,6 +1,8 @@
 package kpits.notif_msoc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +15,38 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 
 public class PertanyaanActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    SharedPreferences pref;
+
+    private final OkHttpClient client = new OkHttpClient();
+    private String idAnswer;
+    private String URLdash = "http://notif-msoc.esy.es/api/v1/send_report";
+    private String sToken = "";
+    private String idUser = "";
+    private String idResponse = "";
+    private String idDetail = "";
+
+    private int hour = 0;
+    private int day = 0;
+    private int week = 0;
+    private int month = 0;
+    private int year = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +55,15 @@ public class PertanyaanActivity extends BaseActivity
 
         getLayoutInflater().inflate(R.layout.activity_pertanyaan, frameLayout);
 
+        pref = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+
         /**
          * Setting title
          */
         setTitle("Pertanyaan");
+
+        sToken = pref.getString("sToken", null);
+        idUser = pref.getString("idUser", null);
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -40,8 +71,8 @@ public class PertanyaanActivity extends BaseActivity
         /*PopulatePerangkatJaringan();
         addItemsOnSpinner3();
         addItemsOnSpinner4();
-        addItemsOnSpinner5();
-        addListenerOnButton();*/
+        addItemsOnSpinner5();*/
+        addListenerOnButton();
         addListenerOnSpinnerItemSelection();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -133,6 +164,7 @@ public class PertanyaanActivity extends BaseActivity
 
     private Spinner spinner1, spinner2, spinner3, spinner4, spinner5, spinner6, spinner7;
     private Button btnSubmit;
+    private TextView textView5;
 
     // add items into spinner dynamically
     public void PopulatePerangkatJaringan() {
@@ -673,6 +705,7 @@ public class PertanyaanActivity extends BaseActivity
                 }
                 else if(sp1.contentEquals("Lainnya")) {
                     PopulateLainnya();
+                    idAnswer = "711";
                 }
             }
 
@@ -688,6 +721,7 @@ public class PertanyaanActivity extends BaseActivity
             public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
                                        long arg3) {
                 // TODO Auto-generated method stub
+                String sp1= String.valueOf(spinner1.getSelectedItem());
                 String sp2= String.valueOf(spinner2.getSelectedItem());
                 spinner3.setVisibility(View.VISIBLE);
                 Toast.makeText(PertanyaanActivity.this, sp2, Toast.LENGTH_SHORT).show();
@@ -709,66 +743,101 @@ public class PertanyaanActivity extends BaseActivity
                 }
                 else if(sp2.contentEquals("Transmission Backbone")) {
                     PopulateTransBackbone();
+                    idAnswer = "211";
                 }
                 else if(sp2.contentEquals("FO Lastmile")) {
                     PopulateFOLastmile();
+                    idAnswer = "221";
                 }
                 else if(sp2.contentEquals("HUB Vsat")) {
                     PopulateHUB();
+                    idAnswer = "231";
                 }
                 else if(sp2.contentEquals("Backhaul")) {
                     PopulateBackhaul();
+                    idAnswer = "241";
                 }
                 else if(sp2.contentEquals("Wareline")) {
                     PopulateWareline();
+                    idAnswer = "251";
                 }
                 else if(sp2.contentEquals("Listrik Padam")) {
                     PopulateListrikPadam();
+                    idAnswer = "311";
                 }
                 else if(sp2.contentEquals("SIAK Konsolidasi")) {
                     PopulateKonsolidasi();
+                    idAnswer = "411";
                 }
                 else if(sp2.contentEquals("Aplikasi SIAK")) {
                     PopulateAplikasiSIAK();
+                    idAnswer = "421";
                 }
                 else if(sp2.contentEquals("Server SIAK")) {
                     PopulateServerSIAK();
+                    idAnswer = "431";
                 }
                 else if(sp2.contentEquals("Listrik")) {
                     PopulateListrik();
+                    idAnswer = "511";
                 }
                 else if(sp2.contentEquals("Relokasi")) {
                     PopulateRelokasi();
+                    idAnswer = "521";
                 }
                 else if(sp2.contentEquals("Renovasi")) {
                     PopulateRenovasi();
+                    idAnswer = "531";
                 }
                 else if(sp2.contentEquals("Kerusakan Bangunan")) {
                     PopulateKerusakan();
+                    idAnswer = "541";
                 }
                 else if(sp2.contentEquals("Server")) {
                     PopulateServer();
+                    idAnswer = "611";
                 }
                 else if(sp2.contentEquals("Switch")) {
                     PopulateSwitch();
+                    idAnswer = "621";
                 }
                 else if(sp2.contentEquals("UPS E-KTP")) {
                     PopulateUPSKTP();
+                    idAnswer = "631";
                 }
                 else if(sp2.contentEquals("Finger Print")) {
                     PopulateFingerPrint();
+                    idAnswer = "641";
                 }
                 else if(sp2.contentEquals("Kamera")) {
                     PopulateKamera();
+                    idAnswer = "651";
                 }
                 else if(sp2.contentEquals("Printer")) {
                     PopulatePrinter();
+                    idAnswer = "661";
                 }
                 else if(sp2.contentEquals("Aplikasi")) {
                     PopulateAplikasi();
+                    idAnswer = "671";
                 }
                 else if(sp2.contentEquals("Lainnya")) {
                     PopulateLainnya2();
+                    if(sp1.contentEquals("Perangkat Jaringan")) {
+                        idAnswer = "161";
+                    }
+                    else if(sp1.contentEquals("Link")) {
+                        idAnswer = "261";
+                    }
+                    else if(sp1.contentEquals("SIAK")) {
+                        idAnswer = "441";
+                    }
+                    else if(sp1.contentEquals("Kantor")) {
+                        idAnswer = "551";
+                    }
+                    else if(sp1.contentEquals("Perangkat E-KTP")) {
+                        idAnswer = "681";
+                    }
                 }
             }
 
@@ -784,36 +853,45 @@ public class PertanyaanActivity extends BaseActivity
             public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
                                        long arg3) {
                 // TODO Auto-generated method stub
+                String sp2= String.valueOf(spinner2.getSelectedItem());
                 String sp3= String.valueOf(spinner3.getSelectedItem());
                 spinner3.setVisibility(View.VISIBLE);
                 Toast.makeText(PertanyaanActivity.this, sp3, Toast.LENGTH_SHORT).show();
 
                 if(sp3.contentEquals("Kabupaten")) {
-
+                    idAnswer = "111";
                 }
                 else if(sp3.contentEquals("Non Kabupaten")) {
-
+                    idAnswer = "112";
                 }
                 else if(sp3.contentEquals("Vsat")) {
-
+                    idAnswer = "121";
                 }
                 else if(sp3.contentEquals("ADSL / IndiHome")) {
-
+                    idAnswer = "122";
                 }
                 else if(sp3.contentEquals("L2S")) {
-
+                    idAnswer = "123";
                 }
                 else if(sp3.contentEquals("G Phone")) {
-
+                    idAnswer = "124";
                 }
                 else if(sp3.contentEquals("UTP")) {
-
+                    idAnswer = "141";
                 }
                 else if(sp3.contentEquals("FO")) {
-
+                    idAnswer = "142";
                 }
                 else if(sp3.contentEquals("Lainnya")) {
-
+                    if(sp2.contentEquals("Modem")) {
+                        idAnswer = "125";
+                    }
+                    else if(sp2.contentEquals("Antena")) {
+                        idAnswer = "132";
+                    }
+                    else if(sp2.contentEquals("Kabel")) {
+                        idAnswer = "143";
+                    }
                 }
             }
 
@@ -831,6 +909,9 @@ public class PertanyaanActivity extends BaseActivity
                 // TODO Auto-generated method stub
                 String sp4= String.valueOf(spinner4.getSelectedItem());
                 Toast.makeText(PertanyaanActivity.this, sp4, Toast.LENGTH_SHORT).show();
+                if(sp4.substring(0, 1).matches("[0-9]")) {
+                    day = Integer.parseInt(sp4.substring(0, 1))*24;
+                }
             }
 
             @Override
@@ -847,6 +928,9 @@ public class PertanyaanActivity extends BaseActivity
                 // TODO Auto-generated method stub
                 String sp5= String.valueOf(spinner5.getSelectedItem());
                 Toast.makeText(PertanyaanActivity.this, sp5, Toast.LENGTH_SHORT).show();
+                if(sp5.substring(0, 1).matches("[0-9]")) {
+                    week = Integer.parseInt(sp5.substring(0, 1))*24*7;
+                }
             }
 
             @Override
@@ -863,6 +947,9 @@ public class PertanyaanActivity extends BaseActivity
                 // TODO Auto-generated method stub
                 String sp6= String.valueOf(spinner6.getSelectedItem());
                 Toast.makeText(PertanyaanActivity.this, sp6, Toast.LENGTH_SHORT).show();
+                if(sp6.substring(0, 1).matches("[0-9]")) {
+                    month = Integer.parseInt(sp6.substring(0, 1))*24*30;
+                }
             }
 
             @Override
@@ -879,6 +966,9 @@ public class PertanyaanActivity extends BaseActivity
                 // TODO Auto-generated method stub
                 String sp7= String.valueOf(spinner7.getSelectedItem());
                 Toast.makeText(PertanyaanActivity.this, sp7, Toast.LENGTH_SHORT).show();
+                if(sp7.substring(0, 1).matches("[0-9]")) {
+                    year = Integer.parseInt(sp7.substring(0, 1))*24*365;
+                }
             }
 
             @Override
@@ -887,26 +977,61 @@ public class PertanyaanActivity extends BaseActivity
 
             }
         });
-
     }
 
+    public void sendResponse() throws Exception {
+        printHours();
+        idResponse = String.valueOf(hour);
+        RequestBody formBody = new FormBody.Builder()
+                .add("token", sToken)
+                .add("id_user", idUser)
+                .add("id_answer", idAnswer)
+                .add("response", idResponse)
+                .add("detail", idDetail)
+                .build();
+        Request request = new Request.Builder()
+                .url(URLdash)
+                .post(formBody)
+                .build();
+
+        // Execute the request and retrieve the response.
+        // TODO: 8/1/2016 add conn error handler
+        Response response = client.newCall(request).execute();
+        ResponseBody body = response.body();
+        String json = body.string();
+
+        Toast.makeText(PertanyaanActivity.this, json, Toast.LENGTH_SHORT).show();
+        Toast.makeText(PertanyaanActivity.this, idDetail, Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+    }
+
+    public void printHours(){
+        textView5 = (TextView) findViewById(R.id.textView5);
+        idDetail = textView5.getText().toString();
+        hour = day + week + month + year;
+    }
 
     // get the selected dropdown list value
     public void addListenerOnButton() {
 
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        spinner3 = (Spinner) findViewById(R.id.spinner3);
-        spinner4 = (Spinner) findViewById(R.id.spinner4);
-        spinner5 = (Spinner) findViewById(R.id.spinner5);
-        spinner6 = (Spinner) findViewById(R.id.spinner6);
-        spinner7 = (Spinner) findViewById(R.id.spinner7);
+//        spinner1 = (Spinner) findViewById(R.id.spinner1);
+//        spinner2 = (Spinner) findViewById(R.id.spinner2);
+//        spinner3 = (Spinner) findViewById(R.id.spinner3);
+//        spinner4 = (Spinner) findViewById(R.id.spinner4);
+//        spinner5 = (Spinner) findViewById(R.id.spinner5);
+//        spinner6 = (Spinner) findViewById(R.id.spinner6);
+//        spinner7 = (Spinner) findViewById(R.id.spinner7);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnSubmit.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                try {
+                    sendResponse();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
                 Toast.makeText(PertanyaanActivity.this,
                         "OnClickListener : " +
