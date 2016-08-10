7897d40c5fca90fef1587b9e2d32e4a993bd6684
Herdianto Naufal Farras <fherdiantonaufal@yahoo.com>
2016-08-10 12:00:06 +0700

    Edit Content Profile
    Need Get User Profile from Database

--- app/src/main/java/kpits/notif_msoc/ProfileActivity.java
+++ app/src/main/java/kpits/notif_msoc/ProfileActivity.java
@@ -3,18 +3,41 @@ package kpits.notif_msoc;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class ProfileActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private loadPage mPageTask = null;
+    private final OkHttpClient client = new OkHttpClient();
+
+    private String sToken = "";
+    private String idUser = "";
+
+    private static final String TAG = "ProfileActivity";
 //    private final int SELECT_PHOTO = 1;
 //    private ImageView imageView;
 
@@ -26,12 +49,15 @@ public class ProfileActivity extends BaseActivity
         SharedPreferences pref = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         getLayoutInflater().inflate(R.layout.activity_profile, frameLayout);
 
+        sToken = pref.getString("sToken", null);
+        idUser = pref.getString("idUser", null);
+
         /**
          * Setting title
          */
         setTitle("Profile");
 
-        TextView textView = (TextView) findViewById(R.id.textView7);
+        TextView textView = (TextView) findViewById(R.id.textView1);
         textView.setText(pref.getString("mUser", null));
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -60,6 +86,12 @@ public class ProfileActivity extends BaseActivity
 //
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        navigationView.setNavigationItemSelectedListener(this);
+        try {
+            mPageTask = new loadPage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mPageTask.execute((Void) null);
     }
 
     @Override
@@ -122,5 +154,73 @@ public class ProfileActivity extends BaseActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class loadPage extends AsyncTask<Void, Void, Boolean> {
+        private final String URLdash = "http://notif-msoc.esy.es/api/v1/get_user";
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                loadload();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mPageTask = null;
+
+            if (!success) {
+                showErrorPage();
+            }
+        }
+
+        private void loadload() throws Exception {
+            RequestBody formBody = new FormBody.Builder()
+                    .add("token", sToken)
+                    .add("id_user", idUser)
+                    .build();
+            Request request = new Request.Builder()
+                    .url(URLdash)
+                    .post(formBody)
+                    .build();
+
+            // Execute the request and retrieve the response.
+            // TODO: 8/1/2016 add conn error handler
+            Response response = client.newCall(request).execute();
+            ResponseBody body = response.body();
+            String json = body.string();
+
+            Log.d(TAG, "respon send " + String.valueOf(response));
+            Log.d(TAG, "isi send " + json);
+
+            HashMap<String, String> map = new HashMap<>();
+            JSONObject jObject = new JSONObject(json);
+            Iterator<?> keys = jObject.keys();
+
+            try {
+                while( keys.hasNext() ){
+                    String key = (String)keys.next();
+                    String value = jObject.getString(key);
+                    map.put(key, value);
+                }
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(ProfileActivity.this, json, Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+        }
+
+        // Displays an error if the app is unable to load content.
+        private void showErrorPage() {
+            Toast.makeText(ProfileActivity.this, "Maaf, mohon coba lagi.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
